18fe9008147d4c421fce20d51788180e60f992e3
Naveen Kumar Dongre <naveenkumardongre1@gmail.com>
2023-04-19 02:43:09 +0530

    Recursive approach added

--- Array/replaceAll0'sWith5.py
+++ Array/replaceAll0'sWith5.py
@@ -1,5 +1,5 @@
 # Problem Statement:
-# You are given an integer N. 
+# You are given an integer N.
 # You need to convert all zeroes of N to 5.
 # Input:
 # N = 1004
@@ -8,24 +8,40 @@
 # on replacing all zeroes with "5", the new
 # number will be "1554".
 
-def convertFive(n):
+# Iterative Approach
+def convertFive1(n):
     # Code here
-    # Using a Stack data structure 
+    # Using a Stack data structure
     stk = []
     ans = 0
-    
+
     # Filling the stack from the end digits of the number
     while n:
-        stk.append(n%10)
-        n = n // 10 
+        stk.append(n % 10)
+        n = n // 10
     # Poping element from the stack and simultaneously building answer
     while stk:
         ele = stk.pop()
         if ele == 0:
             ans = (ans*10) + 5
         else:
-            ans = (ans*10) + ele 
-    return ans 
+            ans = (ans*10) + ele
+    return ans
+
+# Recursive Approach
+
+
+def convertFive(n):
+
+    if n == 0:
+        return 0
+
+    digit = n % 10
+    if digit == 0:
+        digit = 5
+
+    return convertFive(n//10)*10 + digit
+
 
 if __name__ == "__main__":
     n = int(input())
